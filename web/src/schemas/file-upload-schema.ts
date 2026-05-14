c1671eafdc703e044a194feaba7f718390cc87ad
hotequil <hotequil@protonmail.com>
2026-05-14 09:39:37 -0300

    feat(schemas): add schemas for form

---
+++ web/src/schemas/file-upload-schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+import { StellarHelper, SUPPORTED_TOKENS } from 'fractapay-shared'
+
+export const fileUploadSchema = z.object({
+  token: z.enum(SUPPORTED_TOKENS),
+  destinationAddress: z
+    .string()
+    .refine(value => value === '' || StellarHelper.isValidAddress(value), {
+      message: 'error',
+    }),
+})
+
+export type TFileUploadFormValues = z.infer<typeof fileUploadSchema>
